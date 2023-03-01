ebd1bfc7071300b95356e487ee2027191dfbce24
xiangcitoZ <pzxiang1@gmail.com>
2023-03-01 12:37:40 +0100

    Add project files.

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSession.Helpers;
+using MvcCoreSession.Models;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        int numero = 1;
+
+        public IActionResult Index()
+        {
+            ViewData["NUMERO"] = "Valor del número: " + this.numero;
+            return View();
+        }
+
+        public IActionResult SessionSimple(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora",
+                        DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    ViewData["USUARIO"] =
+                        HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] =
+                        HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+
+        public IActionResult SessionPersona(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    Persona persona = new Persona();
+                    persona.Nombre = "Alumno";
+                    persona.Email = "alumno@gmail.com";
+                    persona.Edad = 25;
+                    //DEBEMOS CONVERTIR EL OBJETO PERSONA
+                    //A BYTE[] PARA ALMACENARLO EN SESSION
+                    byte[] data = 
+                        HelperBinarySession.ObjectToByte(persona);
+                    //ALMACENAMOS EL OBJETO BINARIO EN SESSION
+                    HttpContext.Session.Set("PERSONA", data);
+                    ViewData["MENSAJE"] = "Datos almacenados";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //EXTRAEMOS EL OBJETO PERSONA DESDE LOS BYTES[]
+                    //DE SESSION
+                    byte[] data = HttpContext.Session.Get("PERSONA");
+                    //CONVERTIMOS EL BINARIO A OBJETO
+                    Persona persona = (Persona)
+                        HelperBinarySession.ByteToObject(data);
+                    ViewData["PERSONA"] = persona;
+                }
+            }
+            return View();
+        }
+
+        public IActionResult ColeccionSessionPersonas(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    List<Persona> personas = new List<Persona>
+             {
+                 new Persona{ Nombre = "Lucia"
+                 , Email="lucia@gmail.com", Edad = 20},
+                 new Persona{ Nombre = "Andres"
+                 , Email="andres@gmail.com", Edad = 40},
+                 new Persona{ Nombre = "Adrian"
+                 , Email="adrian@gmail.com", Edad = 23}
+             };
+                    byte[] data = HelperBinarySession.ObjectToByte(personas);
+                    HttpContext.Session.Set("LISTAPERSONAS", data);
+                    ViewData["MENSAJE"] = "Colección almacenada";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    byte[] data = HttpContext.Session.Get("LISTAPERSONAS");
+                    List<Persona> personas = (List<Persona>)
+                        HelperBinarySession.ByteToObject(data);
+                    return View(personas);
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MvcCoreSession/Helpers/HelperBinarySession.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MvcCoreSession.Helpers
+{
+    public class HelperBinarySession
+    {
+        //TENDREMOS DOS METODO STATIC
+        //UNO PARA CONVERTIR OBJECT A BINARY
+        public static byte[] ObjectToByte(Object objeto)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, objeto);
+                return stream.ToArray();
+            }
+        }
+
+        //OTRO METODO PARA RECUPERAR UN BINARY A OBJECT
+        public static Object ByteToObject(byte[] data)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                stream.Write(data, 0, data.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                Object objeto = (Object)formatter.Deserialize(stream);
+                return objeto;
+            }
+        }
+    }
+}
